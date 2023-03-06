4b370597ede0c429ab139acdd5d80c04400c480a
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-03-06 20:45:53 +0500

    Add controls js

--- script.js
+++ script.js
@@ -5,15 +5,19 @@ const nextBtn = document.getElementById('next');
 
 let isPlaying = false;
 
-function playSong() {
+function playSong() {  
   isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause');
   music.play();
 }
 
 function pauseSong() {
   isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
   music.pause();
 }
 
-
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
 
