cd93ebc86b2a5d6597df8def514b40522d0e2f9b
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-03-07 13:44:50 +0500

    Add js

--- script.js
+++ script.js
@@ -107,6 +107,16 @@ const updateProgressBar = (e) => {
   }
 }
 
-prevBtn.addEventListener('click', prevSong)
-nextBtn.addEventListener('click', nextSong)
-music.addEventListener('timeupdate', updateProgressBar)
+const setProgressBar = (e) => {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const { duration } = music;
+
+  music.currentTime = (clickX / width) * duration;
+}
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong)
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar)
