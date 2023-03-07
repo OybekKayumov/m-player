e0469d4aa18a9987c58c6ea64792c890e03ff6ec
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-03-07 13:55:48 +0500

    Add play

--- script.js
+++ script.js
@@ -2,31 +2,36 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
-const progressContainer = document.getElementById('progress-container')
-const progress = document.getElementById('progress')
 const currentTimeEl = document.getElementById('current-time');
 const durationEl = document.getElementById('duration');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 
 const songs = [
   {
-    name: 'music-1',
+    name: 'jacinto-1',
     displayName: 'Electric Chill Machine',
-    artist: 'Modern Design'
+    artist: 'Jacinto Design',
   },
   {
-    name: 'music-2',
-    displayName: 'Electric Chill Machine 2',
-    artist: 'Modern Design'
+    name: 'jacinto-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Jacinto Design',
   },
   {
-    name: 'music-3',
-    displayName: 'Electric Chill Machine 3',
-    artist: 'Modern Design'
+    name: 'jacinto-3',
+    displayName: 'Goodnight, Disco Queen',
+    artist: 'Jacinto Design',
   },
-]
+  {
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto Design',
+  },
+];
 
 let isPlaying = false;
 
@@ -50,15 +55,15 @@ playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
 function loadSong(song) {
   title.textContent = song.displayName;
   artist.textContent = song.artist;
-  music.src = `music/${song.name}.mp3`
-  image.src = `img/${song.name}.jpg`
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
 }
 
 let songIndex = 0;
 
 const nextSong = () => {
   songIndex++;
-  if (songIndex > songs.length -1 ) {
+  if (songIndex > songs.length -1) {
     songIndex = 0;
   }
   loadSong(songs[songIndex]);
@@ -68,12 +73,12 @@ const nextSong = () => {
 const prevSong = () => {
   songIndex--;
 
-  if (songIndex < 0 ) {
+  if (songIndex < 0) {
     songIndex = songs.length -1;
   }
 
   loadSong(songs[songIndex]);
-  playSong()
+  playSong();
 }
 
 // on load select first song
@@ -81,7 +86,7 @@ loadSong(songs[songIndex]);
 
 const updateProgressBar = (e) => {
   if (isPlaying) {
-    const {duration, currentTime} = e.srcElement;
+    const { duration, currentTime } = e.srcElement;
 
     // update progress bar
     const progressPercent = (currentTime / duration) * 100;
@@ -90,7 +95,7 @@ const updateProgressBar = (e) => {
     const durationMinutes = Math.floor(duration / 60);
     let durationSeconds = Math.floor(duration % 60);
     if (durationSeconds < 10) {
-      durationSeconds = `0${durationSeconds}`
+      durationSeconds = `0${durationSeconds}`;
     }
 
     // delay NaN
@@ -101,22 +106,21 @@ const updateProgressBar = (e) => {
     const currentMinutes = Math.floor(currentTime / 60);
     let currentSeconds = Math.floor(currentTime % 60);
     if (currentSeconds < 10) {
-      currentSeconds = `0${currentSeconds}`
+      currentSeconds = `0${currentSeconds}`;
     }
-    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`
+    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
   }
 }
 
-const setProgressBar = (e) => {
+function setProgressBar(e) {
   const width = this.clientWidth;
   const clickX = e.offsetX;
   const { duration } = music;
-
   music.currentTime = (clickX / width) * duration;
 }
 
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
-music.addEventListener('ended', nextSong)
+music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
-progressContainer.addEventListener('click', setProgressBar)
+progressContainer.addEventListener('click', setProgressBar);
