596f5c3561b23d5bfe146b09a58dc4008ce43f14
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-03-07 12:15:47 +0500

    Add js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const progressContainer = document.getElementById('progress-container')
 const progress = document.getElementById('progress')
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -80,11 +82,18 @@ loadSong(songs[songIndex]);
 const updateProgressBar = (e) => {
   if (isPlaying) {
     const {duration, currentTime} = e.srcElement;
-    console.log(': ', duration, currentTime);
 
     // update progress bar
     const progressPercent = (currentTime / duration) * 100;
     progress.style.width = `${progressPercent}%`;
+    const durationMinutes = Math.floor(duration / 60);
+    let durationSeconds = Math.floor(duration % 60);
+    if (durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`
+    }
+
+    durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+
   }
 }
 
