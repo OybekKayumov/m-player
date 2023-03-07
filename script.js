4d222da2c0a195f7deb1b6a09a981bb9eb93e172
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-03-07 11:19:40 +0500

    Add js

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+const songs = [
+  {
+    name: 'music-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Modern Design'
+  },
+  {
+    name: 'music-2',
+    displayName: 'Electric Chill Machine 2',
+    artist: 'Modern Design'
+  },
+  {
+    name: 'music-3',
+    displayName: 'Electric Chill Machine 3',
+    artist: 'Modern Design'
+  },
+]
 
 let isPlaying = false;
 
